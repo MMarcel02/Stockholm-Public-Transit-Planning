f70f449442f657998290bb045cc9b089b8e55064
MMarcel02 <marciniak.marc@gmail.com>
2026-04-10 22:21:57 +0200

    RaptorRoute setup

--- src/main/java/com/team18/routing/raptor/RaptorDataStore.java
+++ src/main/java/com/team18/routing/raptor/RaptorDataStore.java
@@ -1,5 +1,8 @@
 package com.team18.routing.raptor;
 
+
+// RAPTOR uses primitive arrays to be cache efficient, which will greatly speedup our route calculation time
+
 public class RaptorDataStore {
     
 }
