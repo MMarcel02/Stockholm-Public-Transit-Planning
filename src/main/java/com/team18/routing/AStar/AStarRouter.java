a78623fceabd659f6c517a0afe6e58dd7c7273f9
Ariamis613 <v.valchev@student.maastrichtuniversity.nl>
2026-04-21 15:26:56 +0200

    Added A* folder in routing

---
+++ src/main/java/com/team18/routing/AStar/AStarRouter.java
@@ -0,0 +1,5 @@
+package com.team18.routing.AStar;
+
+public class AStarRouter {
+    
+}
---
+++ src/main/java/com/team18/routing/AStar/TransitGraph.java
@@ -0,0 +1,54 @@
+package com.team18.routing.AStar;
+
+import java.util.Map;
+import java.util.List;
+import java.util.ArrayList;
+
+import com.team18.model.Edge;
+import com.team18.parser.GTFSParser;
+import com.team18.model.Trip;
+import com.team18.model.StopTime;
+import com.team18.util.GeoCalculator;
+
+public class TransitGraph {
+    Map<String, List<Edge>> adjacency = new java.util.HashMap<>();
+
+    private final double WALKING_SPEED = 83.33;
+
+    private void build(GTFSParser parser){
+        //Main method
+        System.err.println("Building the graph... ");
+
+        buildTransitEdges(parser);
+    }
+
+    private void buildTransitEdges(GTFSParser parser){
+        for(Trip trip : parser.trips.values()){
+            List<StopTime> stopTimes = new ArrayList<>(trip.stopTimes);
+
+            for(int i = 0; i < stopTimes.size() - 1; ++i){
+                StopTime current = stopTimes.get(i);
+                StopTime next = stopTimes.get(i + 1);
+
+                //Get total travel time
+                int travelTime = next.arrivalTime - current.departureTime;
+
+                double walkingTime = (GeoCalculator.calculateEquirectangularDistance(current.stop.lat, current.stop.lon, next.stop.lat, next.stop.lon) / WALKING_SPEED) / 60.0;
+
+                if (travelTime < 0) continue;
+
+                Edge edge = new Edge(next.stop, "transit",  trip.tripId, current.departureTime, travelTime, walkingTime);
+
+                List<Edge> edges = new ArrayList<Edge>();
+                edges.add(edge);
+
+                adjacency.put(current.stop.id, edges);
+            }
+        }
+    }
+
+    private void buildWalkingEdges(GTFSParser parser){
+
+    }
+
+}
