5c82a0df85db24629ead2d19a8ee78787b077715
Ariamis613 <v.valchev@student.maastrichtuniversity.nl>
2026-04-21 15:22:47 +0200

    Added walk time in seconds

--- src/main/java/com/team18/model/Edge.java
+++ src/main/java/com/team18/model/Edge.java
@@ -7,12 +7,14 @@ public class Edge {
     public String tripId;
     public int departureTime;
     public int travelTimeSeconds;
+    public double travelWalkTimeSeconds;
 
-    public Edge(Stop dest, String mode, String tripId, int departureTime, int travelTimeSeconds) {
+    public Edge(Stop dest, String mode, String tripId, int departureTime, int travelTimeSeconds, double travelWalkTimeSeconds) {
         this.dest = dest;
         this.mode = mode;
         this.tripId = tripId;
         this.departureTime = departureTime;
         this.travelTimeSeconds = travelTimeSeconds;
+        this.travelWalkTimeSeconds = travelWalkTimeSeconds;
     }
 }
