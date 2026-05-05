20cd70434a9abfe1aab3fe4f8d5af59bd43e7cfc
MMarcel02 <marciniak.marc@gmail.com>
2026-05-05 16:11:05 +0200

    Added back walking method analysis for haversine vs equi

--- src/main/java/com/team18/RoutingEngine.java
+++ src/main/java/com/team18/RoutingEngine.java
@@ -6,25 +6,17 @@ import com.team18.routing.raptor.RaptorAlgorithm;
 import com.team18.routing.raptor.RaptorBuilder;
 import com.team18.routing.raptor.RaptorNetwork;
 import com.team18.routing.Router;
-
-// Needs to stay in this folder and with this title as defined in the project manual
-
-import com.team18.util.GeoCalculator;
 import com.team18.util.ParsingUtil;
 
-import java.io.BufferedReader;
 import java.io.EOFException;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.nio.file.NoSuchFileException;
-import java.util.Enumeration;
 import java.util.List;
 import java.util.Map;
 import java.util.zip.*;
-import java.io.InputStream;
 
 import com.leastfixedpoint.json.JSONReader;
 import com.leastfixedpoint.json.JSONSyntaxError;
@@ -56,13 +48,6 @@ public class RoutingEngine {
             if (json instanceof Map<?,?>) {
                 Map<?,?> request = (Map<?,?>) json;
 
-                // Just a sanity check to make sure routing engine is processing requests
-                // should be removed in final version as spec only wants load and route requests
-                if (request.containsKey("ping")) {
-                    sendOk(Map.of("pong", request.get("ping")));
-                    continue;
-                }
-
                 // Helpful resource
                 //https://www.baeldung.com/java-read-zip-files
                 if (request.containsKey("load")) {
@@ -86,8 +71,6 @@ public class RoutingEngine {
                     } 
                 }
 
-                // Crow flight calculator for now using both formulas to test for accuracy 
-                // for accuracy for the speedup we can feed lots of routes and only look at the last few (as JVM needs to warm up)
                 if (request.containsKey("routeFrom") && request.containsKey("to") && request.containsKey("startingAt")) {
                     if (this.raptorNetwork == null) {
                         sendError("Raptor network not loaded, check that 'load' request was sent earlier.");
@@ -113,47 +96,6 @@ public class RoutingEngine {
                         }
                         
                         sendOk(routeSteps);
-
-                        // for (RouteStep routeStep : journey) {
-                        //     Map<String, Object> routeStep = new java.util.LinkedHashMap<>();
-                        //     routeStep.put("mode", "walk");
-                        //     routeStep.put("to", toNode);
-                        //     routeStep.put("startTime", startTime);
-    
-                        //     boolean isDebug = request.containsKey("debug") && request.get("debug").equals("true");
-                            
-                        //     if (isDebug) {
-                        //         // this is the mode in which we can compare different approaches
-                        //         long startHaversine = System.nanoTime();
-                        //         double distanceMetersHaversine = GeoCalculator.calculateHaversineDistance(latFrom, lonFrom, latTo, lonTo);
-                        //         long endHaversine = System.nanoTime();
-                        //         long timeHaversineNs = endHaversine - startHaversine;
-        
-                        //         long startEqui = System.nanoTime();
-                        //         double distanceMetersEqui = GeoCalculator.calculateEquirectangularDistance(latFrom, lonFrom, latTo, lonTo);
-                        //         long endEqui = System.nanoTime();
-                        //         long timeEquiNs = endEqui - startEqui;
-        
-                        //         int walkMinutesHaversine = (int) Math.round(distanceMetersHaversine / WALKING_SPEED);
-                                
-                        //         double errorPercentage = Math.abs(distanceMetersHaversine - distanceMetersEqui) / distanceMetersHaversine * 100.0;
-        
-                        //         double speedMultiplier = 0;
-                        //         if (timeEquiNs > 0) {
-                        //             speedMultiplier = (double) timeHaversineNs / timeEquiNs;
-                        //         }
-                                
-                        //         routeStep.put("duration", walkMinutesHaversine);
-                        //         routeStep.put("DEBUG_error_percent", errorPercentage);
-                        //         routeStep.put("DEBUG_speedup", speedMultiplier);
-                        //     } else {
-                        //         // this is the default we will use in production
-                        //         double distanceMetersEqui = GeoCalculator.calculateEquirectangularDistance(latFrom, lonFrom, latTo, lonTo);
-                        //         routeStep.put("duration", (int) Math.round(distanceMetersEqui / WALKING_SPEED));
-                        //     }
-                        // }
-
-
                     } catch (ClassCastException | NullPointerException e) {
                         sendError("Coordinates must be formatted as numbers");
                     } catch (Exception e) {
