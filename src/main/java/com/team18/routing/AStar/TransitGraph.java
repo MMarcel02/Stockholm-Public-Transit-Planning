7b64bd5fcd0db5cc3f63c4d7270f5fe0ba079e08
Velislav Valchev <v.valchev@student.maastrichtuniversity.nl>
2026-05-28 15:03:37 +0200

    Added buildWalkingEdges() to the main method

--- src/main/java/com/team18/routing/AStar/TransitGraph.java
+++ src/main/java/com/team18/routing/AStar/TransitGraph.java
@@ -21,6 +21,7 @@ public class TransitGraph {
         System.err.println("Building the graph... ");
 
         buildTransitEdges(parser);
+        buildWalkingEdges(parser);
     }
 
     public void buildTransitEdges(GTFSParser parser){
