da3a7026f20305c58332247bbf081e8e1237bb5e
Ștefan Ciobanu <stefan.ciobanu@student.maastrichtuniversity.nl>
2026-06-01 13:53:35 +0200

    added poplatlon.csv

--- src/main/java/com/team18/App.java
+++ src/main/java/com/team18/App.java
@@ -3,6 +3,9 @@ package com.team18;
 import com.team18.gui.GuiApp;
 import com.team18.parser.PopdistParser;
 import java.io.IOException;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
 
 public class App 
 {
@@ -13,6 +16,16 @@ public class App
 		try {
 			PopdistParser parser = new PopdistParser();
 			parser.loadFromCsv("data/stockholm/pop.csv");
+
+			File output = new File("data/stockholm/poplatlon.csv");
+			BufferedWriter bufw = new BufferedWriter(new FileWriter(output));
+
+			bufw.write("lat,lon,population\n");
+			for (PopdistParser.Point pt : parser.points) {
+				bufw.write(String.format("%f,%f,%d\n", pt.lat, pt.lon, pt.population));
+			}
+
+			bufw.close();
 		} catch (IOException ex) {}
 	}
 }
