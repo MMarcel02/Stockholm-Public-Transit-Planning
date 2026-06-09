e3e09aafa5473c93b47d5db37f35f8eb266c049d
MMarcel02 <marciniak.marc@gmail.com>
2026-06-09 21:21:05 +0200

    Added comment about the outer bounding box

--- src/main/java/com/team18/util/StockholmUrbanArea.java
+++ src/main/java/com/team18/util/StockholmUrbanArea.java
@@ -7,12 +7,14 @@ public class StockholmUrbanArea {
     // Visualise here: https://bboxfinder.com/#59.207929,17.744295,59.447169,18.393180
     // Remove unnecessary data in sl dataset that falls outside of this region 
 
+
+    // Changed to be the same as inner coordinates because of the edge cases with routes
     public static final double OUTER_MIN_LAT = 59.223832;
     public static final double OUTER_MAX_LAT = 59.432507;
     public static final double OUTER_MIN_LON = 17.779999;
     public static final double OUTER_MAX_LON = 18.358842;
 
-    // Smaller bounding box for inner urban area on which we will calculate heatmap and optimizer
+    // Bounding box for inner urban area on which we will calculate heatmap and optimizer
     // Visualise here: https://bboxfinder.com/#59.223832,17.779999,59.432507,18.358842
     // To see both go to the above link, click on 'Enter coordinates' button and paste this:
     // 17.655029,59.159045,18.483104,59.510634
