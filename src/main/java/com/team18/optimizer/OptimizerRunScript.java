a83cabbb36dcdb330d1439279e384a231b395275
MMarcel02 <marciniak.marc@gmail.com>
2026-06-09 18:26:14 +0200

    last test stuff

--- src/main/java/com/team18/optimizer/OptimizerRunScript.java
+++ src/main/java/com/team18/optimizer/OptimizerRunScript.java
@@ -1,8 +1,5 @@
 package com.team18.optimizer;
 
-import java.io.FileWriter;
-import java.util.Map;
-
 import com.team18.parser.GTFSParser;
 import com.team18.parser.PopdistParser;
 import com.team18.routing.raptor.RaptorBuilder;
@@ -25,39 +22,39 @@ public class OptimizerRunScript
 			Optimizer optimizer = new Optimizer(network, parser.demandPointCoordinates, parser.demand);
 
 			// ONE RUN FOR ALL ROUTES
-			long curr = System.currentTimeMillis();
-			Map<String, Double> routeToCostImpact = optimizer.getRouteRemovedToCostImpact(Config.REPRESENTATIVE_WEEKDAY);
-			long end = System.currentTimeMillis();
-			System.err.println("Time for 1 map calc (mins): " + ((end - curr) / 60000.0));
-			
-			FileWriter wr = new FileWriter("data/costs/run_final_all_routes.csv");
-			wr.write("routeId,cost\n");
-			for (String routeId : routeToCostImpact.keySet()) {
-				Double value = routeToCostImpact.get(routeId);
-				wr.write(String.format("%s,%f\n", routeId, value));
-			}
-			wr.close();
-			
-			// BEST 3 With GREEDY
 			// long curr = System.currentTimeMillis();
-			// Map<String, Double> bestRoutesToDisable = optimizer.multiThreadedOptimize();
+			// Map<String, Double> routeToCostImpact = optimizer.getRouteRemovedToCostImpact(Config.REPRESENTATIVE_WEEKDAY);
 			// long end = System.currentTimeMillis();
-			// System.err.println("Time for 3 map calcs (mins): " + ((end - curr) / 60000.0));
-
-			// FileWriter wr = new FileWriter("data/costs/run_final_3_best.csv");
+			// System.err.println("Time for 1 map calc (mins): " + ((end - curr) / 60000.0));
+			
+			// FileWriter wr = new FileWriter("data/costs/run_final_all_routes.csv");
 			// wr.write("routeId,cost\n");
-			// for (String routeId : bestRoutesToDisable.keySet()) {
-			// 	Double value = bestRoutesToDisable.get(routeId);
+			// for (String routeId : routeToCostImpact.keySet()) {
+			// 	Double value = routeToCostImpact.get(routeId);
 			// 	wr.write(String.format("%s,%f\n", routeId, value));
 			// }
 			// wr.close();
+			
+			// BEST 3 With GREEDY
+			long curr = System.currentTimeMillis();
+			Map<String, Double> bestRoutesToDisable = optimizer.multiThreadedOptimize();
+			long end = System.currentTimeMillis();
+			System.err.println("Time for 3 map calcs (mins): " + ((end - curr) / 60000.0));
+
+			FileWriter wr = new FileWriter("data/costs/run_final_3_best.csv");
+			wr.write("routeId,cost\n");
+			for (String routeId : bestRoutesToDisable.keySet()) {
+				Double value = bestRoutesToDisable.get(routeId);
+				wr.write(String.format("%s,%f\n", routeId, value));
+			}
+			wr.close();
 
 			// ONE THAT OUTPUTS OTHER DATA TO TO USE IN THE ANALYZER
 			// USE FOR FINDING GOOD RATIO + MULTIPLIER
-			// wr = new FileWriter("data/costs/run_final_all_routes_testing.csv");
-			// curr = System.currentTimeMillis();
+			// FileWriter wr = new FileWriter("data/costs/run_final_all_routes_testing.csv");
+			// long curr = System.currentTimeMillis();
 			// Map<String, double[]> routeToCostImpactTESTING = optimizer.getRouteCostImpactForAnalysis(Config.REPRESENTATIVE_WEEKDAY);
-			// end = System.currentTimeMillis();
+			// long end = System.currentTimeMillis();
 
 			// System.err.println("Time for one map calc (mins): " + ((end - curr) / 60000.0));
 			// wr.write("routeId,routeCost,costIncreasePassenger,cost\n");
